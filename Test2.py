778ffac634e9d25bc42373c30c125db753f789fc
Thiago Ramos Linux <devthiagoramos@outlook.com>
2024-04-06 15:45:40 -0300

    alterado test2

--- Test2.py
+++ Test2.py
@@ -1,4 +1,1 @@
-#Arquivo criado com o objetivo de inicializar um repositorio com algum conteudo
-nome = "Thiago"
-sobrenome = "Ramos"
-print("Nome completo " + nome + " " + sobrenome)
+#Esse sera somente um teste
